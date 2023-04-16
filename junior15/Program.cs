7e854127023fe830c9ae090c9b4ff0c90bafa66f
Danila <asd@mail.ru>
2023-04-16 13:08:09 +0300

    Добавьте файлы проекта.

---
+++ junior15/Program.cs
@@ -0,0 +1,20 @@
+﻿string password = "1234";
+string secretMessage = "Тайное сообщение";
+
+int attempt = 3;
+
+for (int i = 0; i < attempt; i++)
+{
+    Console.Write("Введите пароль: ");
+    string passwordEntry = Console.ReadLine();
+
+    if (passwordEntry == password)
+    {
+        Console.WriteLine(secretMessage);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Не верный пароль");
+    }
+}
